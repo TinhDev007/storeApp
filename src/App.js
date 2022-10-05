34018b006fd5fcfd9263a5a02ac69dcfa1033270
TinhDev007 <tindt2222@gmail.com>
2022-10-06 00:35:34 +0700

    sync change

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
 import { Container, Row, Col } from "react-bootstrap";
 import ProductCard from "./components/ProductCard";
 import './App.css';
-import data from "./data.json";
+// import data from "./data.json";
+
+const url = "https://appstore.decisions.com/decisions/Primary/restapi/Flow/08daa21d-f3b7-7acd-0cd3-7603700325cf?sessionid=NS-08daa2f1-f3e7-a032-0cd3-76037003629e&outputtype=JSON";
+
 
 function App() {
+  const [data, setData] = useState([]);
+
+  const getData = async () => {
+    await axios.get(`${url}`).then((response) => {
+      setData(response?.data?.Done?.OUTPUTS);
+    }).catch(error => console.log(`Error: ${error}`));
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+
   return (
     <div className="App">
       <div className="page-content">
-        <Container>        
-          <h1 className="section-title">Monitors</h1>
+        <Container>
           <Row>
             {data.map((product, index) => (
               <Col md={6} lg={4} xl={3} className="mb-4" key={index}>
                 <ProductCard product={product} />
               </Col>
-            ))}          
+            ))}
           </Row>
         </Container>
       </div>          
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,23 +1,26 @@
 import { Card } from "react-bootstrap";
 
 const ProductCard = ({ product }) => {
+    console.log('ddd', product)
     return (
-        <a href={product.link} target="_blank" rel="noreferrer" className="card-link">
+        <a href={product.VideoLink} target="_blank" rel="noreferrer" className="card-link">
             <Card>
                 <div className="card-image">
-                    <img src={product.iconurl} alt="" />
+                    <img src={`data:image/jpeg;base64, ${product.IconData}`} alt="" />
                 </div>
-                <Card.Body>
-                    <Card.Title>{product.itemname}</Card.Title>
+                <Card.Body> 
+                    <Card.Title>{product.Name}</Card.Title>
                     <Card.Text>
                         {product.itemcost}
                     </Card.Text>
                     <Card.Text>                        
-                        {product.itemdescription}
+                        {product.Description}
                     </Card.Text>
-                    <Card.Text style={{fontWeight: 'bold', fontSize: 14}}>                        
-                        Version: {product.itemversion}
-                    </Card.Text>                  
+                    {product.SupportedVersion && (
+                        <Card.Text style={{fontWeight: 'bold', fontSize: 14}}>                        
+                            Version: {product.SupportedVersion}
+                        </Card.Text>                  
+                    )}                    
                 </Card.Body>
             </Card>
         </a>        
